13735563a645838a83b5339d29c729cf1d684433
gabriel-brunetti <gabriel.brunetti1@gmail.com>
2019-10-14 09:31:52 -0300

    chore: initial css and structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div className="container">
+        <span className="logo" >Logo</span>
+        <h1>iFollow</h1>
+        <form>
+          <input type='text' placeholder="Digite seu usuário do github" />
+          <button>Entrar</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
